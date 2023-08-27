3cb4b03ee3ccb96c61361318740c688c5cd151fd
khdoj <nouhailakartoych321@gmail.com>
2023-08-27 06:57:46 +0000

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,7 +11,6 @@
 int is_digit(char *s)
 {
 int i = 0;
-
 while (s[i])
 {
 if (s[i] < '0' || s[i] > '9')
@@ -30,7 +29,6 @@ return (1);
 int _strlen(char *s)
 {
 int i = 0;
-
 while (s[i] != '\0')
 {
 i++;
@@ -58,7 +56,6 @@ int main(int argc, char *argv[])
 {
 char *s1, *s2;
 int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
-
 s1 = argv[1], s2 = argv[2];
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
